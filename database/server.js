041f5f7e909cfcd7a21b88294653bfe6f8d3cf0e
Juan Ochoa <juan.ochoa@adira.com.mx>
2020-11-25 17:06:31 -0600

    Reintentar

--- database/server.js
+++ database/server.js
@@ -1,8 +1,8 @@
 const { Client } = require('pg')
-//const host = "top-express-dev.cxsn0wvppdrz.us-east-2.rds.amazonaws.com"
-//const password = "Jiog040719"
-const password = (process.env.password || "9462");
-const host = (process.env.host || "juan8a.local");
+const host = "top-express-dev.cxsn0wvppdrz.us-east-2.rds.amazonaws.com"
+const password = "Jiog040719"
+//const password = (process.env.password || "9462");
+//const host = (process.env.host || "juan8a.local");
 console.log("Pass: ", password)
 console.log("Host: ", host)
 const client = new Client({
@@ -13,14 +13,28 @@ const client = new Client({
     "database": "topExpress"
 })
 
+
+
+async function start(){
+    await connect();
+}
+
+async function connect(){
+    try{
+        await client.connect();
+        console.log("Connection succesful!")
+    } catch(e) {
+        console.error(`TOPEXPRESSERROR: Failed to connect ${e}`)
+    }
+}
 function sql() {
-    this.connect = function() {
+    this.connect2 = function() {
         return new Promise((resolve, reject) => reject("error connecting"));
     }
 }
 
 
-function connect() {
+function connect2() {
     return new Promise((resolve, reject) => {
         // const poolPromise = new sql.ConnectionPool("config.db");
         const poolPromise = new sql();
@@ -38,7 +52,7 @@ function connect() {
 }
 
 function establishConnection() {
-     var a = connect();
+     var a = connect2();
      a.then(a => console.log("success"))
     .catch(err => {
         console.error("Retrying");
@@ -46,8 +60,8 @@ function establishConnection() {
         setTimeout(establishConnection, 1000 * 60 * 60 * 12);
     });
 };
-
-establishConnection();
+start() 
+//establishConnection();
 
 function statusOperation(statusOperationCode, description, errors, data){
     return{
